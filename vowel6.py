79690c742f2500c5a8912ee10fc2a74dd899c5fd
kristynf <kristynf@bu.edu>
2020-04-22 13:32:08 -0400

    add program

--- vowel6.py
+++ vowel6.py
@@ -10,6 +10,7 @@ found = {}
 # found['u'] = 0
 for letter in word:
     if letter in vowels:
+        found.setdefault(letter, 0)
         found[letter] += 1
 for k, v in sorted(found.items()):
     print(k, 'was found', v, 'time(s).')
