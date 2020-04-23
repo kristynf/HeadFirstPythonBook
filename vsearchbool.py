afd407bc3c18badc4cedaf536fba46d8a650e2d1
kristynf <kristynf@bu.edu>
2020-04-23 16:42:31 -0400

    updated programs

--- vsearchbool.py
+++ vsearchbool.py
@@ -1,7 +1,6 @@
 def search4vowels():
-    """Use to display any vowel found in any search term"""
+    """Return a boolean depending if any vowel found in any search term"""
     vowels = set('aeiou')
     word = input('Provide a term to search for vowels: ')
     found = vowels.intersection(set(word))
-    for vowel in found:
-        print(vowel)
+    return bool(found)
