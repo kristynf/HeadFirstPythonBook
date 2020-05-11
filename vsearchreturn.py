e5c401cd4009882b75d0e9347941be0d92b308b2
kristynf <kristynf@bu.edu>
2020-05-11 14:17:14 -0400

    added program

--- vsearchreturn.py
+++ vsearchreturn.py
@@ -1,5 +1,4 @@
 def search4vowels(word):
     """Return a boolean depending if any vowel found in any search term"""
     vowels = set('aeiou')
-    found = vowels.intersection(set(word))
-    return bool(found)
+    return vowels.intersection(set(word))
